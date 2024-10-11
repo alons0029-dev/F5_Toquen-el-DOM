245bbbd74abdf6da58fe67d9df5a410f5d0b71b4
unknown <formacio@BARCELO-5KA7TO7.formacio.local>
2024-10-11 12:10:09 +0200

    ocilador agregado y trabanado en ello

--- scripts/piano.js
+++ scripts/piano.js
@@ -1,161 +1,173 @@
 var context= new AudioContext();
- const doNote = document.getElementById("DoN")
- doNote.addEventListener("click", doN)
-function doN(){
+ const doNote = document.getElementById("doN")
+ const doSosNote = document.getElementById("doSosN")
+ const reNote = document.getElementById("reN")
+ const reSosNote = document.getElementById("reSosN")
+ const miNote = document.getElementById("miN")
+ const faNote = document.getElementById("faN")
+ const faSosNote = document.getElementById("faSosN")
+ const solNote = document.getElementById("solN")
+ const solSosNote = document.getElementById("solSosN")
+ const laNote = document.getElementById("laN")
+ const laSosNote = document.getElementById("laSosN")
+ const siNote = document.getElementById("siN")
+ const do8Note = document.getElementById("do8N")
+
+ doNote.addEventListener("click", do_Note)
+ doSosNote.addEventListener("click", doSos_Note)
+ reNote.addEventListener("click", re_Note)
+ reSosNote.addEventListener("click", reSos_Note)
+ miNote.addEventListener("click", mi_Note)
+ faNote.addEventListener("click", fa_Note)
+ faSosNote.addEventListener("click", faSos_Note)
+ solNote.addEventListener("click", sol_Note)
+ solSosNote.addEventListener("click", solSos_Note)
+ laNote.addEventListener("click", la_Note)
+ laSosNote.addEventListener("click", laSos_Note)
+ siNote.addEventListener("click", si_Note)
+ do8Note.addEventListener("click", do8_Note)
+
+
+let volumen = document.getElementById('volumen').value
+let volumenValue = parseFloat(volumen)
+
+// Tipos de osciladores: 'sine', 'square', 'sawtooth', 'triangle'
+function do_Note(volumenValue){
+
+        let o= context.createOscillator();
+        g=context.createGain();
+        o.connect(g);
+        o.type="sawtooth";
+        o.frequency.value=130.81;
+        g.connect(context.destination);
+        o.start(0);
+        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+        console.log("funciona");
+
+    }
+
+ function doSos_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=261;
+        o.frequency.value=138.59;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
+function re_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=293;
+        o.frequency.value=146.83;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function mi(){
+function reSos_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=329;
+        o.frequency.value=155.56;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function fa(){
+function mi_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=349;
+        o.frequency.value=164.81;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function sol(){
+function fa_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=391;
+        o.frequency.value=174.61;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function la(){
+function faSos_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=440;
+        o.frequency.value=185.00;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function si(){
+function sol_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=493;
+        o.frequency.value=196.00;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
+function solSos_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=293;
+        o.frequency.value=207.65;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
+function la_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=293;
+        o.frequency.value=220.00;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
+function laSos_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=293;
+        o.frequency.value=233.08;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
+function si_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
         o.type="sawtooth";
-        o.frequency.value=293;
+        o.frequency.value=246.94;
         g.connect(context.destination);
         o.start(0);
         g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
         console.log("funciona")
     }
-function re(){
-        var o= context.createOscillator();
-        g=context.createGain();
-        o.connect(g);
-        o.type="sawtooth";
-        o.frequency.value=293;
-        g.connect(context.destination);
-        o.start(0);
-        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
-        console.log("funciona")
-    }
-function re(){
-        var o= context.createOscillator();
-        g=context.createGain();
-        o.connect(g);
-        o.type="sawtooth";
-        o.frequency.value=293;
-        g.connect(context.destination);
-        o.start(0);
-        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
-        console.log("funciona")
-    }
-function re(){
-        var o= context.createOscillator();
-        g=context.createGain();
-        o.connect(g);
-        o.type="sawtooth";
-        o.frequency.value=293;
-        g.connect(context.destination);
-        o.start(0);
-        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
-        console.log("funciona")
-    }
-function re(){
+function do8_Note(){
         var o= context.createOscillator();
         g=context.createGain();
         o.connect(g);
